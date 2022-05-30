e7a03edb7e0777b3ec21ce1539af870e20b8597a
Muhmmad Shahzaib <93182457+ShahzaibArgon@users.noreply.github.com>
2022-05-31 00:57:09 +0500

    expo
    expo APP

---
+++ App.js
@@ -0,0 +1,133 @@
+import React, { useRef, useState,useEffect } from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Modal,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import {
+  getModel,
+  convertBase64ToTensor,
+  startPrediction,
+} from './src/helpers/tensor-helper';
+import { cropPicture } from './src/helpers/image-helper';
+import { Camera ,CameraType} from 'expo-camera';
+const RESULT_MAPPING = ['Triangle', 'Circle', 'Square'];
+const Main = () => {
+  const cameraRef = useRef();
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [presentedShape, setPresentedShape] = useState('');
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(()=>{
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  },[])
+
+
+  const handleImageCapture = async () => {
+    setIsProcessing(true);
+    const imageData = await cameraRef.current.takePictureAsync({
+      base64: true,
+    });
+    processImagePrediction(imageData);
+  };
+
+  const processImagePrediction = async (base64Image) => {
+    const croppedData = await cropPicture(base64Image, 300);
+    const model = await getModel();
+    const tensor = await convertBase64ToTensor(croppedData.base64);
+    console.log(tensor)
+    const prediction = await startPrediction(model, tensor);
+    const highestPrediction = prediction.indexOf(
+      Math.max.apply(null, prediction),
+    );
+    setPresentedShape(RESULT_MAPPING[highestPrediction]);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Modal visible={isProcessing} transparent={true} animationType="slide">
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <Text>Your current shape is {presentedShape}</Text>
+            {presentedShape === '' && <ActivityIndicator size="large" />}
+            <Pressable
+              style={styles.dismissButton}
+              onPress={() => {
+                setPresentedShape('');
+                setIsProcessing(false);
+              }}>
+              <Text>Dismiss</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        autoFocus={true}
+        whiteBalance={Camera.Constants.WhiteBalance.auto}></Camera>
+      <Pressable
+        onPress={() => handleImageCapture()}
+        style={styles.captureButton}></Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  camera: {
+    width: '100%',
+    height: '100%',
+  },
+  captureButton: {
+    position: 'absolute',
+    left: Dimensions.get('screen').width / 2 - 50,
+    bottom: 40,
+    width: 100,
+    zIndex: 100,
+    height: 100,
+    backgroundColor: 'white',
+    borderRadius: 50,
+  },
+  modal: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 300,
+    borderRadius: 24,
+    backgroundColor: 'gray',
+  },
+  dismissButton: {
+    width: 150,
+    height: 50,
+    marginTop: 60,
+    borderRadius: 24,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
+
+export default Main;
